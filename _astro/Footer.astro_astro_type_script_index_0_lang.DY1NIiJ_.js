f301b58b5764793e8c5cb35536460dae431eb17a
Orielsy Diaz <orielsy@gmail.com>
2026-09-19 02:34:18 -0400

    Updates

--- _astro/Footer.astro_astro_type_script_index_0_lang.DY1NIiJ_.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./analytics.BYERs4jP.js";document.getElementById(`footer-email-contact`)?.addEventListener(`click`,()=>{e(`contact_email`,{source:`footer`});let t=`contact@${[`orielsy`,`com`].join(`.`)}`;window.location.href=`mailto:${t}`});
